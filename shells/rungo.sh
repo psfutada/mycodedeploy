2ef5517dfe5e7f9c784e1da6b6f1f657bb9fddee
psfutada <tafutada@pascaliaasia.com>
2016-07-06 07:53:39 +0900

    fix go

--- shells/rungo.sh
+++ shells/rungo.sh
@@ -8,8 +8,9 @@ export PATH=$PATH:$GOROOT/bin
 export GOPATH=/home/ec2-user/work
 
 # Restart Go server
-sudo killall go
-go run $GOPATH/main.go > /dev/null 2> /dev/null < /dev/null &
+sudo killall nor
+go install nor
+$GOPATH/bin/nor > /dev/null 2> /dev/null < /dev/null &
 
 # Restart Nginx
 sudo service nginx restart
